4e37a7d9db3f88a0666d5601a1981cc808210f4f
qiao <2476941134@qq.com>
2023-04-23 18:39:12 +0800

    使用Spring Cache缓存套餐及菜品数据

--- src/main/java/com/itheima/reggie/ReggieAoolication.java
+++ src/main/java/com/itheima/reggie/ReggieAoolication.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.event.TransactionalEventListener;
 import sun.applet.Main;
@@ -14,8 +15,9 @@ import sun.applet.Main;
  */
 @Slf4j
 @SpringBootApplication
-@ServletComponentScan
-@EnableTransactionManagement
+@ServletComponentScan          //Servlet（控制器）、Filter（过滤器）、Listener（监听器）可以直接通过@WebServlet、@WebFilter、@WebListener注解自动注册到Spring容器中，无需其他代码。
+@EnableTransactionManagement   //开启事务注解功能
+@EnableCaching                 //开启缓存Spring cache注解功能
 public class ReggieAoolication {
     public static void main(String[] args) {
         SpringApplication.run(ReggieAoolication.class,args);
